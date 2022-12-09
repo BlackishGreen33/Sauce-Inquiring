80798156e214756d157eacb913a7908bb4dce2f5
墨綠BG <s5460703@gmail.com>
2022-12-09 13:54:53 +0800

    1.0.2

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const bntEM = document.getElementById('bnt');
+const linkAEM = document.getElementById('linkA');
+const linkBEM = document.getElementById('linkB');
 
 bntEM.addEventListener('click', function () {
     let nHentaiEM = document.getElementById('title1').value;
@@ -9,4 +11,12 @@ bntEM.addEventListener('click', function () {
     if (wnacgEM == '') {
         location.href = 'https://nhentai.net/g/' + nHentaiEM + '/';
     }
+});
+
+linkAEM.addEventListener('click', function () {
+    alert('学术研究中，梯子是您必要的工具，而该工具请自备。');
+});
+
+linkBEM.addEventListener('click', function () {
+    alert('低能儿，以为天上会掉鸡腿？');
 });
