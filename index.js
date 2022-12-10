8ae0a7861972982d21ef27d1b679b24ec8a8af97
墨綠BG <s5460703@gmail.com>
2022-12-10 10:29:07 +0800

    1.0.3

--- index.js
+++ index.js
@@ -5,11 +5,15 @@ const linkBEM = document.getElementById('linkB');
 bntEM.addEventListener('click', function () {
     let nHentaiEM = document.getElementById('title1').value;
     let wnacgEM = document.getElementById('title2').value;
-    if (nHentaiEM == '') {
-        location.href = 'https://www.wnacg.org/photos-index-aid-' + wnacgEM + '.html';
+    let R18comicEM = document.getElementById('title3').value;
+    if (nHentaiEM == '' && wnacgEM =='') {
+        location.href = 'https://18comic.org/photo/' + R18comicEM;
     }
-    if (wnacgEM == '') {
+    if (R18comicEM == '' && wnacgEM =='') {
         location.href = 'https://nhentai.net/g/' + nHentaiEM + '/';
+    }
+    if (nHentaiEM == '' && R18comicEM == '') {
+        location.href = 'https://www.wnacg.org/photos-index-aid-' + wnacgEM + '.html';
     }
 });
 
